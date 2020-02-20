072a53ae81158e99af1d01dfcf860169397d999f
Qay-Tekkie <gkay260@gmail.com>
2020-02-20 18:29:26 +0100

    Modified index.js

--- store/index.js
+++ store/index.js
@@ -1,13 +1,19 @@
 export const state = () => ({
-  profiles: [
-    {
-      name: 'Gift Egwuenu',
-      image:
-        'https://res.cloudinary.com/lauragift/image/upload/w_300,h_300,q_auto/v1562153429/gift5_irlhir.jpg',
-      role: 'Frontend Engineer',
-      Zodiac: 'Taurus',
-      twitter: 'https://twitter.com/lauragift_',
-      linkedin: 'https://www.linkedin.com/in/egwuenugift/'
-    }
-  ]
+    profiles: [{
+            name: 'Gift Egwuenu',
+            image: 'https://res.cloudinary.com/lauragift/image/upload/w_300,h_300,q_auto/v1562153429/gift5_irlhir.jpg',
+            role: 'Frontend Engineer',
+            Zodiac: 'Taurus',
+            twitter: 'https://twitter.com/lauragift_',
+            linkedin: 'https://www.linkedin.com/in/egwuenugift/'
+        },
+        {
+            name: 'Keren-happuch Achoja',
+            image: 'NA',
+            role: 'Frontend Developer Intern',
+            Zodiac: 'Aquarus',
+            twitter: 'https://twitter.com/go_qay',
+            linkedin: 'https://www.linkedin.com/in/achojakay'
+        }
+    ]
 })
