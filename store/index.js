12ae9853be98f5e59685956f32a4adb5f4c5632d
Shulammite-Aso <asoamarachi@gmail.com>
2020-02-22 02:04:45 +0330

    include details about Amarachi Aso

--- store/index.js
+++ store/index.js
@@ -8,6 +8,15 @@ export const state = () => ({
       Zodiac: 'Taurus',
       twitter: 'https://twitter.com/lauragift_',
       linkedin: 'https://www.linkedin.com/in/egwuenugift/'
+    },
+    {
+      name: 'Amarachi Shulammite Aso',
+      image:
+        'https://avatars.io/twitter/AsoAmarachi',
+      role: 'Beginner software Engineer',
+      Zodiac: 'https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/160/facebook/158/shrug_1f937.png',
+      twitter: 'https://twitter.com/AsoAmarachi',
+      linkedin: ''
     }
   ]
 })
