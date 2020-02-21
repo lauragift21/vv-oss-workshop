67841511c4995cc2fec01d1a325954e49ac7cbb6
Orung <magnificentorung@gmail.com>
2020-02-21 04:55:32 +0100

    promise orung updated page

--- store/index.js
+++ store/index.js
@@ -1,13 +1,13 @@
 export const state = () => ({
   profiles: [
     {
-      name: 'Gift Egwuenu',
+      name: 'Promise Orung',
       image:
         'https://res.cloudinary.com/lauragift/image/upload/w_300,h_300,q_auto/v1562153429/gift5_irlhir.jpg',
-      role: 'Frontend Engineer',
+      role: 'Frontend Developer',
       Zodiac: 'Taurus',
-      twitter: 'https://twitter.com/lauragift_',
-      linkedin: 'https://www.linkedin.com/in/egwuenugift/'
+      twitter: 'https://twitter.com/Promiseorung',
+      linkedin: 'https://www.linkedin.com/in/promise-orung-a3a495136/'
     }
   ]
 })
