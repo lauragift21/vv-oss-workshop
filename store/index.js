292d7c03e6bdfcacda9c44f99c360db0c6079d0a
Dammyton <owolabidamilola98@gmail.com>
2020-02-24 15:06:08 +0100

    Add Damilola Owolabi to Contributors list

--- store/index.js
+++ store/index.js
@@ -1,13 +1,13 @@
 export const state = () => ({
   profiles: [
     {
-      name: 'Gift Egwuenu',
+      name: 'Damilola Owolabi',
       image:
-        'https://res.cloudinary.com/lauragift/image/upload/w_300,h_300,q_auto/v1562153429/gift5_irlhir.jpg',
-      role: 'Frontend Engineer',
-      Zodiac: 'Taurus',
-      twitter: 'https://twitter.com/lauragift_',
-      linkedin: 'https://www.linkedin.com/in/egwuenugift/'
+        'https://res.cloudinary.com/dyyj2hjf5/image/upload/v1582552627/IMG-20200218-WA0019_ofsal1.jpg',
+      role: 'Software Engineer',
+      Zodiac: 'Virgo',
+      twitter: 'https://twitter.com/Dammyton16',
+      linkedin: 'https://www.linkedin.com/in/damilola-owolabi-7821b714a/'
     }
   ]
 })
